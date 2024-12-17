63637d7f4324cd3bf747396c0dfbdb44cf8e6160
sleepynut <nut.pa@kbtg.tech>
2024-12-17 16:22:47 +0700

    add comment

--- internal/student/endpoint.go
+++ internal/student/endpoint.go
@@ -32,6 +32,7 @@ func (s *StudentEndpoint) CalculateGrade(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, fmt.Sprintf("0 <= score <= 100,but got %d", score))
 	}
 
+	// business logic
 	grade := studentCalculateGrade(score)
 
 	// response score
