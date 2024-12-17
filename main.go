7f442727af4430342514bc7ffc305f5ce611999e
sleepynut <nut.pa@kbtg.tech>
2024-12-17 15:55:50 +0700

    user viper config to read dev config

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/sleepiinuts/kbtg-go-prj/internal/app"
 	"github.com/sleepiinuts/kbtg-go-prj/internal/handler"
 	"github.com/sleepiinuts/kbtg-go-prj/internal/ping"
 )
 
 func main() {
+
+	c := app.Config{}
+	if err := c.Init("dev"); err != nil {
+		log.Printf("err: %v\n", err)
+	}
+
+	log.Printf("config: %+v\n", c)
 	echoHTTP()
 }
 
