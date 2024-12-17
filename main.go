fdcdf6bc15aa68d699f480dc634afb0cbe60e686
sleepynut <nut.pa@kbtg.tech>
2024-12-17 11:04:18 +0700

    echo framework intro

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/labstack/echo"
 	"github.com/sleepiinuts/kbtg-go-prj/internal/ping"
 )
 
 func main() {
-	fmt.Println("hello world")
+	echoHTTP()
+}
 
+func pureHTTP() {
 	// GET: ping
 	http.HandleFunc("/ping", ping.PingHandler)
 
 	http.HandleFunc("/hello", ping.HelloHandler)
 	http.ListenAndServe(":8080", nil)
+}
 
+func echoHTTP() {
+	e := echo.New()
+	e.GET("/ping", func(ctx echo.Context) error {
+		return ctx.String(http.StatusOK, "pong")
+	})
+
+	e.Start(":8080")
 }
