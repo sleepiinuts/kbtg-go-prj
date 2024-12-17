13215e1e7d7baeb2c092f8c3b86cb8bcadc1f67f
sleepynut <nut.pa@kbtg.tech>
2024-12-17 10:59:10 +0700

    move all endpoints to internal

--- internal/ping/endpoint.go
+++ internal/ping/endpoint.go
@@ -1,6 +1,10 @@
 package ping
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func PingHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
@@ -9,4 +13,38 @@ func PingHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Add("Content-type", "application/json")
 	w.Write([]byte("pong"))
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("panic: %v", err)
+		}
+	}()
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	// expect json body
+	var req struct {
+		Name    string `json:"name"`
+		Surname string `json:"surname"`
+		Address struct {
+			No   int    `json:"no"`
+			Road string `json:"road"`
+		} `json:"address"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Header.Add need to be put before content writing?
+	w.Header().Add("Content-type", "application/json")
+	if err := json.NewEncoder(w).Encode(req); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -14,44 +13,7 @@ func main() {
 	// GET: ping
 	http.HandleFunc("/ping", ping.PingHandler)
 
-	//
-
-	http.HandleFunc("/hello", HelloHandler)
-	http.HandleFunc("/hello2", HelloHandler)
+	http.HandleFunc("/hello", ping.HelloHandler)
 	http.ListenAndServe(":8080", nil)
 
-}
-
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Printf("panic: %v", err)
-		}
-	}()
-
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	// expect json body
-	var req struct {
-		Name    string `json:"name"`
-		Surname string `json:"surname"`
-		Address struct {
-			No   int    `json:"no"`
-			Road string `json:"road"`
-		} `json:"address"`
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Header.Add need to be put before content writing?
-	w.Header().Add("Content-type", "application/json")
-	if err := json.NewEncoder(w).Encode(req); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 }
