336b9f5507d76afdfe51d12a0d4aa3ce0f0682f2
sleepynut <nut.pa@kbtg.tech>
2024-12-17 15:55:28 +0700

    intro to config with viper

---
+++ internal/app/config.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	// vn *viper.Viper
+
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+}
+
+func (c *Config) Init(env string) error {
+	vn := viper.New()
+
+	vn.AddConfigPath(filepath.Join(".", "configs"))
+	vn.SetConfigType("yml")
+	vn.SetConfigName(fmt.Sprintf("config.%s.yml", env))
+
+	if err := vn.ReadInConfig(); err != nil {
+		return err
+	}
+
+	if err := vn.Unmarshal(&c); err != nil {
+		return err
+	}
+
+	return nil
+}
