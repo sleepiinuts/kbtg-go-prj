cb68cb55b1b32c4d3d3f2d3c8a069cb3e826ec02
sleepynut <nut.pa@kbtg.tech>
2024-12-17 10:23:20 +0700

    separate hello to HelloHandler

--- main.go
+++ main.go
@@ -21,40 +21,40 @@ func main() {
 
 	//
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				fmt.Printf("panic: %v", err)
-			}
-		}()
-
-		if r.Method != http.MethodPost {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
-		// expect json body
-		var req struct {
-			Name    string `json:"name"`
-			Surname string `json:"surname"`
-			Address struct {
-				No   int    `json:"no"`
-				Road string `json:"road"`
-			} `json:"address"`
-		}
-
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		if err := json.NewEncoder(w).Encode(req); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Add("Content-type", "application/json")
-		// w.Write([]byte(fmt.Sprintf("Hello, %v", req)))
-	})
+	http.HandleFunc("/hello", HelloHandler)
 	http.ListenAndServe(":8080", nil)
 
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("panic: %v", err)
+		}
+	}()
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	// expect json body
+	var req struct {
+		Name    string `json:"name"`
+		Surname string `json:"surname"`
+		Address struct {
+			No   int    `json:"no"`
+			Road string `json:"road"`
+		} `json:"address"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-type", "application/json")
+	if err := json.NewEncoder(w).Encode(req); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
